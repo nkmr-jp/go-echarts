e425357f81c53905c540863110665f0cc957ece5
nkmr-jp <nkmr.git@gmail.com>
2025-01-26 11:40:56 +0900

    Add Left,Top,Right,Bottom,Width,Height to DataZoom

--- opts/data_zoom.go
+++ opts/data_zoom.go
@@ -68,4 +68,36 @@ type DataZoom struct {
 	//'empty': data that outside the window will be set to NaN, which will not lead to changes of windows of other axes.
 	//'none': Do not filter data.
 	FilterMode string `json:"filterMode,omitempty"`
+
+	// Distance between toolbox component and the left side of the container.
+	// left value can be instant pixel value like 20; it can also be a percentage
+	// value relative to container width like '20%'; and it can also be 'left', 'center', or 'right'.
+	// If the left value is set to be 'left', 'center', or 'right', then the component
+	// will be aligned automatically based on position.
+	Left string `json:"left,omitempty"`
+
+	// Distance between toolbox component and the top side of the container.
+	// top value can be instant pixel value like 20; it can also be a percentage
+	// value relative to container width like '20%'; and it can also be 'top', 'middle', or 'bottom'.
+	// If the left value is set to be 'top', 'middle', or 'bottom', then the component
+	// will be aligned automatically based on position.
+	Top string `json:"top,omitempty"`
+
+	// Distance between toolbox component and the right side of the container.
+	// right value can be instant pixel value like 20; it can also be a percentage
+	// value relative to container width like '20%'.
+	// Adaptive by default.
+	Right string `json:"right,omitempty"`
+
+	// Distance between toolbox component and the bottom side of the container.
+	// bottom value can be instant pixel value like 20; it can also be a percentage
+	// value relative to container width like '20%'.
+	// Adaptive by default.
+	Bottom string `json:"bottom,omitempty"`
+
+	// Width of text block
+	Width float32 `json:"width,omitempty"`
+
+	// Height of text block
+	Height float32 `json:"height,omitempty"`
 }
